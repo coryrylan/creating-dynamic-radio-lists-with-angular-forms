022255db78856bb688dcf42ab92bb19a1ad3cdc0
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-22 20:58:31 -0600

    5 files modified, 3 added, 6 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,63 @@
-import './polyfills';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { of } from 'rxjs';
+import 'zone.js';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <a href="">
+      <h1>Dynamic Radio List in Angular</h1>
+    </a>
 
-import { AppModule } from './app/app.module';
+    <form [formGroup]="form" (ngSubmit)="submit()">
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+      <label *ngFor="let order of orders">
+        <input formControlName="orders" type="radio" name="orders" [value]="order.id" />
+        {{order.name}}
+      </label>
+
+      <br>
+      <button>submit</button>
+    </form>
+  `,
+})
+export class App {
+form: FormGroup;
+  orders: any[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      orders: ['']
+    });
+
+    // mimic async orders
+    of(this.getOrders()).subscribe(orders => {
+      this.orders = orders;
+      this.form.controls['orders'].patchValue(this.orders[0].id);
+    });
+
+    // synchronous orders
+    // this.orders = this.getOrders();
+    // this.form.controls.orders.patchValue(this.orders[0].id);
   }
-  window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+  getOrders() {
+    return [
+      { id: 100, name: 'order 1' },
+      { id: 200, name: 'order 2' },
+      { id: 300, name: 'order 3' },
+      { id: 400, name: 'order 4' }
+    ];
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
+
+bootstrapApplication(App);
